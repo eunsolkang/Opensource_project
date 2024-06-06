db894b7e4809072e2a65535db3982348b361ae74
Hierarch <whitekudari@gmail.com>
2024-06-07 03:11:30 +0900

    게임 오버 화면 & 재시작 #3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static bool Game_Over;
-    void Start()
-    {
-        
-    }
+
+    public GameObject resultScreen;
 
     void Update()
     {
-        if (Battery_UI.battery_meter_value <= 0)
+        if (Battery_UI.battery_meter_value <= 0 && !Game_Over)
         {
             Game_Over = true;
-            Debug.Log("Game Over" + Battery_UI.battery_meter_value);
+            var ResultScreen = Instantiate(resultScreen, gameObject.transform.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform) as GameObject;
+            //Debug.Log("Game Over" + Battery_UI.battery_meter_value);
         }
             
     }
---
+++ Assets/Scripts/ResultManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
+
+public class ResultManager : MonoBehaviour
+{
+    private Vector3 targetPos;
+    public float speed;
+    public int dif;
+
+    public TMP_Text scoretext;
+    private bool moveFinished = false;
+    private void Awake()
+    {
+        targetPos = Vector3.zero;
+    }
+    void Start()
+    {
+        scoretext.text = "Score: " + score_UI.score.ToString();
+    }
+
+    void Update()
+    {
+        if(transform.position != targetPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+        else moveFinished = true;
+
+        if(Input.GetMouseButtonDown(0) && moveFinished)
+        {
+           
+           StartCoroutine(Reset());
+        }
+         
+    }
+
+    private IEnumerator Reset()
+    {
+        yield return StartCoroutine(moveDown());
+        score_UI.score = 0;
+        Battery_UI.battery_meter_value = 1;
+        GameManager.Game_Over = false;
+        SceneManager.LoadScene("main");
+    }
+
+    private IEnumerator moveDown()
+    {
+        targetPos.y = -dif;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        yield return new WaitUntil(()=>transform.position == targetPos);
+    }
+}
