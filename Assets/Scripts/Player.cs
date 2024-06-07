21a577a0f0cea8ad35a0d09f8a155e4519593126
rlatkdwns575 <wb001223@gmail.com>
2024-06-07 15:07:20 +0900

    플레이어 경로 생성 #18

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,22 @@ public class Player : MonoBehaviour
     [SerializeField]
 
     public float speed = 3f;
+    [SerializeField]
+    private GameObject Trail;
+    [SerializeField]
+    private Transform TrailTransform;
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
         transform.position += moveTo * speed * Time.deltaTime;
+    
+        TrailRoutine();
     }
 
+    void TrailRoutine(){
+        Instantiate(Trail, TrailTransform.position, Quaternion.identity);
+    }
     
 }
